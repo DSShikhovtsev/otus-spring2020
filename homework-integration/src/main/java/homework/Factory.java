062970b172005d93b4d646b261aa8c0d1f03cbb0
DSShikohvtsev <sexy4upa@gmail.com>
2020-07-21 11:10:13 +0500

    homework: spring integration

---
+++ homework-integration/src/main/java/homework/Factory.java
@@ -0,0 +1,15 @@
+package homework;
+
+import homework.domain.Furniture;
+import homework.domain.Tree;
+import org.springframework.integration.annotation.Gateway;
+import org.springframework.integration.annotation.MessagingGateway;
+
+import java.util.Collection;
+
+@MessagingGateway
+public interface Factory {
+
+    @Gateway(requestChannel = "treeChannel", replyChannel = "furnitureChannel")
+    Collection<Furniture> process(Collection<Tree> typeTree);
+}
---
+++ homework-integration/src/main/java/homework/domain/Furniture.java
@@ -0,0 +1,13 @@
+package homework.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class Furniture {
+
+    private String type;
+}
