29bfae0d64b2160bee30d95aafe794cd3c9f972f
DSShikohvtsev <sexy4upa@gmail.com>
2020-08-28 15:02:31 +0500

    homework: webflux

--- homework-webflux/src/test/java/webflux/controller/AuthorControllerTest.java
+++ homework-webflux/src/test/java/webflux/controller/AuthorControllerTest.java
@@ -1,40 +1,42 @@
 package webflux.controller;
 
-import com.google.gson.Gson;
-import webflux.domain.Author;
-import webflux.service.author.AuthorService;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import webflux.controller.rest.AuthorRestController;
+import webflux.domain.Author;
+import webflux.service.author.AuthorService;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @DisplayName("Controller для работы с авторами")
-@WebMvcTest(AuthorController.class)
-class AuthorControllerTest  {
+@RunWith(SpringRunner.class)
+@WebFluxTest(controllers = AuthorRestController.class)
+@Import(AuthorService.class)
+public class AuthorControllerTest {
 
-    /*@Autowired
-    private MockMvc mvc;
     @MockBean
     private AuthorService service;
+    @Autowired
+    private WebTestClient client;
 
-    @BeforeEach
-    private void setUp() {
+    @Before
+    public void setUp() {
         Mockito.reset(service);
         Author author = new Author("1", "author");
         Author author1 = new Author("2", "author1");
@@ -42,65 +44,59 @@ class AuthorControllerTest  {
         List<Author> list = new ArrayList<>();
         list.add(author);
         list.add(author1);
-        given(service.getAllAuthors()).willReturn(list);
-        given(service.getAuthorById("1")).willReturn(author);
-        given(service.getAuthorById("2")).willReturn(new Author("2", "test"));
-        given(service.getAuthorById("3")).willReturn(author2);
+        given(service.getAllAuthors()).willReturn(Flux.just(author, author1));
+        given(service.getAuthorById("1")).willReturn(Mono.just(author));
+        given(service.getAuthorById("2")).willReturn(Mono.just(new Author("2", "test")));
+        given(service.getAuthorById("3")).willReturn(Mono.just(author2));
     }
 
     @Test
     @DisplayName("возвращать список всех авторов")
-    void showAuthors() throws Exception {
+    public void showAuthors() {
         Author author = new Author("1", "author");
         Author author1 = new Author("2", "author1");
-        List<Author> list = new ArrayList<>();
-        list.add(author);
-        list.add(author1);
-        String json = new Gson().toJson(list);
-        this.mvc.perform(get("/api/authors")
-                .accept(MediaType.APPLICATION_JSON)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(json));
+        client.get()
+                .uri("/api/authors")
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectBodyList(Author.class)
+                .contains(author, author1);
     }
 
     @Test
     @DisplayName("возвращать ожидаемого автора")
-    void showAuthor() throws Exception {
+    public void showAuthor() {
         Author author = new Author("1", "author");
-        String json = new Gson().toJson(author);this.mvc.perform(get("/api/authors/1")
-                .accept(MediaType.APPLICATION_JSON)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(json));
+        client.get()
+                .uri("/api/authors/1")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(Author.class)
+                .value(response ->
+                        assertThat(response).isEqualToComparingFieldByField(author));
     }
 
     @Test
     @DisplayName("добавлять автора в таблицу")
-    void addAuthor() throws Exception {
+    public void addAuthor() {
         Author author = new Author("3", "testInsert");
-        String json = new Gson().toJson(author);
-        this.mvc.perform(post("/api/authors")
+        client.post()
+                .uri("/api/authors")
+                .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(json));
-
+                .body(Mono.just(author), Author.class)
+                .exchange()
+                .expectStatus().isOk();
     }
 
     @Test
-    @DisplayName("обновлять имя автора")
-    void updateAuthor() throws Exception {
-        Author author = new Author("2", "test");
-        Author returned = (Author) Objects.requireNonNull(this.mvc.perform(get("/author?id=2")).andExpect(status().isOk()).andReturn()
-                .getModelAndView()).getModelMap().getAttribute("author");
-        assertEquals(author, returned);
+    @DisplayName("удалять автора из таблицы")
+    public void deleteAuthor() {
+        client.delete()
+                .uri("/api/authors/1")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk();
     }
-
-    @Test
-    @DisplayName("удалять автора")
-    void deleteAuthor() throws Exception {
-        String json = new Gson().toJson(new Author("2", "author1"));
-        this.mvc.perform(MockMvcRequestBuilders
-                .delete("/api/authors/delete")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(json));
-    }*/
 }
